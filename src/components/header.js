ccfdabe91c34c71ac0213a74935e44444ba3b4c1
Darragh Downey <darragh.downey90@gmail.com>
2020-10-17 16:34:00 +1100

    adjusting header.js

--- src/components/header.js
+++ src/components/header.js
@@ -16,40 +16,7 @@ class Header extends React.Component {
               width: "100%",
             }}
           >
-            <Link
-              to="/"
-              title="Home"
-              style={{
-                boxShadow: `none`,
-                textDecoration: `none`,
-                color: `inherit`,
-              }}
-            >
-              <img
-                alt="UI Logo"
-                src="https://contrast.now.sh/cff/40f?size=48&fontSize=2&baseline=2&fontWeight=900&radius=32&text=UI"
-              />
-              <span
-                style={{
-                  position: "absolute",
-                  width: 1,
-                  height: 1,
-                  overflow: "hidden",
-                  top: -9999,
-                }}
-              >
-                <h4>Home</h4>
-              </span>
-            </Link>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              width: "100%",
-            }}
-          >
-            <h4>
+            <h2>
               <Link
                 to="/about"
                 style={{
@@ -61,9 +28,9 @@ class Header extends React.Component {
               >
                 About
               </Link>
-            </h4>
+            </h2>
 
-            <h4>
+            <h2>
               <Link
                 to="/work"
                 style={{
@@ -76,9 +43,9 @@ class Header extends React.Component {
               >
                 Work
               </Link>
-            </h4>
+            </h2>
 
-            <h4>
+            <h2>
               <Link
                 to="/blog"
                 style={{
@@ -90,9 +57,9 @@ class Header extends React.Component {
               >
                 Blog
               </Link>
-            </h4>
+            </h2>
 
-            <h4>
+            <h2>
               <Link
                 to="/contact"
                 style={{
@@ -104,7 +71,7 @@ class Header extends React.Component {
               >
                 Contact
               </Link>
-            </h4>
+            </h2>
           </div>
       </header>
     )
